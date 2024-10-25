0f09952de167a06c64ac3ed4025f869fce681b35
onafiona <farcas.oana99@gmail.com>
2024-10-25 17:23:08 +0300

    Added delete customer without functionality Added Transfer to another user without functionality

---
+++ InternetBanking-FrontEnd/src/main/java/com/example/internetbankingfrontend/controller/Transfer/TransferController.java
@@ -0,0 +1,15 @@
+package com.example.internetbankingfrontend.controller.Transfer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class TransferController {
+    @FXML
+    TextField TransferPerson;
+    @FXML
+    TextField TransferSum;
+    @FXML
+    public void TransferSumofMoney() {
+
+    }
+}
---
+++ InternetBanking-FrontEnd/src/main/java/com/example/internetbankingfrontend/controller/customer/DeleteCustomerController.java
@@ -0,0 +1,13 @@
+package com.example.internetbankingfrontend.controller.customer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class DeleteCustomerController {
+    @FXML
+    TextField CustomerUsername;
+    @FXML
+    public void DeleteCustomer(){
+
+    }
+}
