7aae002871d815bf83b3d1140a03d1c6f90c5577
onafiona <farcas.oana99@gmail.com>
2024-10-31 10:59:18 +0200

    addded transaction history functionality

--- InternetBanking-FrontEnd/src/main/java/com/example/internetbankingfrontend/controller/TransactionHistory/TransactionHistoryController.java
+++ InternetBanking-FrontEnd/src/main/java/com/example/internetbankingfrontend/controller/TransactionHistory/TransactionHistoryController.java
@@ -46,6 +46,53 @@ public class TransactionHistoryController {
 
         // Adăugăm lista de tranzacții în tabel
         transactionTable.setItems(transactionList);
+        loadtransactionshistory();
+    }
+    private void loadtransactionshistory() {
+        String receiverIban = LoginController.user.getIban();
+        String urlString = "http://localhost:8080/api/transfer/all/" + receiverIban;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Accept", "application/json");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                    StringBuilder response = new StringBuilder();
+                    String responseLine;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+                    JSONObject jsonResponse = new JSONObject(response.toString());
+                    JSONArray data = jsonResponse.getJSONArray("data");
+                    System.out.println(data);
+                    transactionList.clear();
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject obj = data.getJSONObject(i);
+                        System.out.println(obj);
+
+                        Transaction transaction = new Transaction(
+                                obj.getInt("id"),                     // id ca Integer
+                                obj.getString("senderIban"),           // senderIban ca String
+                                obj.getString("receiverIban"),         // receiverIban ca String
+                                obj.getDouble("amount"),               // amount ca Double
+                                obj.getString("status")                // status ca String
+                        );
+
+                        transactionList.add(transaction);
+                    }
+
+                }
+            } else {
+
+                System.out.println("Eroare la încărcarea tranzacțiilor: cod " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
     
